39c70ac27411f788ff55ec8b3c76874fed1af504
danielsilva12UA <104782036+danielsilva12UA@users.noreply.github.com>
2024-06-12 18:29:23 +0100

    more plots

--- project2/test.py
+++ project2/test.py
@@ -9,17 +9,18 @@ import dns.reversename
 import pygeoip
 import matplotlib.pyplot as plt 
 
-datafile='dataset8/data8_mod2.parquet'
-data=pd.read_parquet(datafile)
-testfile='dataset8/test8_mod2.parquet'
-test=pd.read_parquet(testfile)
+# datafile='dataset8/data8_mod2.parquet'
+# data=pd.read_parquet(datafile)
+# testfile='dataset8/test8_mod2.parquet'
+# test=pd.read_parquet(testfile)
 serversfile='dataset8/servers8.parquet'
 servers=pd.read_parquet(serversfile)
 
-test.groupby(['timestamp'])[['down_bytes', 'up_bytes']].sum().plot()
+fig, ax = plt.subplots()
+#a0=servers.groupby(['src_ip'])['timestamp'].diff().fillna(0).mean()
+servers['diff_timestamp']=servers.groupby(['src_ip'])['timestamp'].diff().fillna(0)
+servers.groupby(['src_ip'])['diff_timestamp'].max().sort_values(ascending=False).plot(kind='barh', figsize=(10,25))
+#a0.sort_values().plot(kind='barh', figsize=(10,25))
+plt.tight_layout()
 plt.savefig('playing.png')
 plt.close()
-
-a = test.loc[(test['src_ip']=='192.168.108.97')].loc[(test['up_bytes']>=2*(10^8))]['timestamp'].diff().fillna(0).sort_index()
-for x in a.keys():
-    print(x, a[x])
